331cb99fa0478701ed798ac3328efa519b4ecfb0
Salem Eid <salem.eid@gmail.com>
2020-09-25 20:25:43 +0300

    Updates

---
+++ precache-manifest.49a29f5171aaaf02d697f0f09f7f1820.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423a699617519183ff16504bfe223efc",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "ce438fac2579417a5cc7",
+    "url": "/react-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a332e68cf74e33a0eba0",
+    "url": "/react-test/static/js/2.b2454c41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.b2454c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce438fac2579417a5cc7",
+    "url": "/react-test/static/js/main.d2327644.chunk.js"
+  },
+  {
+    "revision": "cf683f5aa7555d75e67d",
+    "url": "/react-test/static/js/runtime-main.df86af06.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
