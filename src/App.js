3ebf54b823f059e78c53a68cc3cb16cd0c9bb180
Salem Eid <salem.eid@gmail.com>
2020-09-25 20:26:20 +0300

    just for testing

--- src/App.js
+++ src/App.js
@@ -2,25 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      meaningOfLife: 47
+    }
+  }
+
+  handleClick = () => {
+    this.setState({meaningOfLife:this.state.meaningOfLife + 1})
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.meaningOfLife}
+          </p>
+          <button onClick = {this.handleClick}>
+            Click Me!
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
